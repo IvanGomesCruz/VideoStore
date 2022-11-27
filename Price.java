a65329a3f3758e1bf81f14eaf464a5afdd04657d
Ivan Gomes <97919328+IvanGomesCruz@users.noreply.github.com>
2022-11-27 23:30:01 +0000

    Commit 13

--- Price.java
+++ Price.java
@@ -1,10 +1,4 @@
 public abstract class Price {
    public abstract int getPriceCode();
-   public double getCharge(int daysRented){
-    double thisAmount = 0;
-    thisAmount += getPriceCode();
-    if (daysRented > getPriceCode())
-      thisAmount += (daysRented - getPriceCode()) * 1.5;        
-    return thisAmount;
-  }
+   public abstract double getCharge(int daysRented);
 }
